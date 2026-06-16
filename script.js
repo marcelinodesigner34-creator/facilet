9f5d618805a440c6b615317a13e10f5a87c7997c
Marcelino Lopes <marcelinodesigner34@gmail.com>
2026-06-15 21:11:30 -0300

    gerei as disciplinas do 7º ano (a) dinamicamente com JS

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ botoesSubmenu.forEach(function(botao){
     });
     
 });
+const ulDisciplinas = document.getElementById("disciplina-7a");
+const disciplinas=["Português", "Matemática", "Ciências", "História", "Geografia", "Inglês", "Artes", "ED.Física", "EN.Religioso" ];
+disciplinas.forEach(function(disciplina){
+const liDisciplinas = `<li class="disciplinas">
+    <button class="btn-disciplinas"><i data-lucide="book-open"></i>${disciplina}<i class="seta" data-lucide="chevron-right"></i></button>
+</li>`
+ulDisciplinas.innerHTML += liDisciplinas
+});
+lucide.createIcons();
+
